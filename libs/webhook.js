c53c8362a95a77861856af8c4cf7d6045b1df05d
pastak <pasta0915@gmail.com>
2016-01-21 02:43:25 +0900

    return value

--- libs/webhook.js
+++ libs/webhook.js
@@ -5,7 +5,6 @@ const releaseTargetBranch = process.env.RELEASE_BRANCH || 'production'
 const github = require('./github')
 
 module.exports = function (webhook, host) {
-  return [webhook.action, webhook.pull_request.base.ref, releaseTargetBranch]
   if (!(webhook.action === 'opened' && webhook.pull_request.base.ref === releaseTargetBranch)) return ''
   github.commentRelaseUrl(webhook, host)
   return 'pr opened'
