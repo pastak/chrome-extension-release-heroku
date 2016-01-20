43969240b2fdd040fcbc010d46eaf22e877b062b
pastak <pasta0915@gmail.com>
2016-01-21 02:13:15 +0900

    first

---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const app = require('koa')()
+const router = require('koa-router')()
+const koaBody = require('koa-body')()
+const redis = require('redis').createClient(process.env.REDIS_URL || 6379)
+const ChromeWebstoreManager = require('chrome-webstore-manager')
+const itemId = process.env.ITEM_ID
+const clientId = process.env.WEBSTORE_CLIENT_ID
+const clientSecret = process.env.WEBSTORE_CLIENT_SECRET
+const webhook = require('./libs/webhook')
+
+const chromeWebstoreManager = new ChromeWebstoreManager(clientId, clientSecret)
+
+router.get('/', function *(next) {
+  this.type = 'text/html'
+  this.body = `This app is receive webhook from github to release chrome webstore<br />
+  <a href='https://chrome.google.com/webstore/detail/${itemId}'>This item's webstore page</a>.`
+})
+
+router.post('/webhook', koaBody, function *(next) {
+  if (this.request.headers['X-Github-Event'] !== 'pull_request') return this.body = ''
+  const reqJSON = JSON.parse(this.request.body)
+  this.body = webhook(reqJSON, this.request.origin)
+})
+
+router.get('/prepare-release/:issueNumber', function *(next) {
+  const callbackUrl = `${this.request.origin}/callback/${this.params.issueNumber}`
+  this.response.redirect(chromeWebstoreManager.getCodeUrl(callbackUrl))
+})
+
+router.get('/callback/:issueNumber', function *(next) {
+  const query = {}
+  this.request.querystring.split('&').forEach((q) => {
+    const tmp = q.split('=')
+    query[tmp[0]] = tmp[1]
+  })
+  const callbackUrl = `${this.request.origin}/callback/${this.params.issueNumber}`
+  this.body = yield chromeWebstoreManager.getAccessToken(query['code'], callbackUrl).then((data) => {
+    redis.set(`token_${this.params.issueNumber}`, data.access_token)
+    return 'Success to prepare your release. This item will be released as soon as merged.'
+  })
+})
+
+router.post('/release/:issueNumber', koaBody, function *(next) {
+  const koa = this
+  const extZipBinData = this.request.body
+  const extZipPath = path.resolve(__dirname, 'extension.zip')
+  this.body = yield new Promise((resolve) => {
+    redis.get(`token_${this.params.issueNumber}`, function (err, value) {
+      chromeWebstoreManager.updateItem(value, extZipBinData, itemId)
+      .then((data) => {
+        chromeWebstoreManager.publishItem(value, itemId).then(() => {
+          resolve({message: 'success'})
+        }).catch((err) => {
+          koa.response.status = 500
+          resolve({message: 'failed to upload', error: err})
+        })
+      }).catch((err) => {
+        koa.response.status = 500
+        resolve({message: 'failed to upload', error: err})
+      })
+    })
+  })
+})
+
+app.use(router.routes())
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
---
+++ libs/webhook.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const releaseTargetBranch = process.env.RELEASE_BRANCH || 'production'
+
+const github = require('./github')
+
+module.exports = function (webhook, host) {
+  if (!(webhook.action === 'opened' && webhook.pull_request.base.ref === releaseTargetBranch)) return ''
+  github.commentRelaseUrl(webhook, host)
+}
