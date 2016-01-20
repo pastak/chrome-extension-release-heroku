1c8775662ab82968064ca732882da636098610e2
pastak <pasta0915@gmail.com>
2016-01-21 02:46:18 +0900

    fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ router.get('/', function *(next) {
 
 // Receive Webhook from GitHub
 router.post('/webhook', koaBody, function *(next) {
-  if (this.request.headers['X-Github-Event'] !== 'pull_request') return this.body = 'not pull_request:' + this.request.headers['X-Github-Event']
+  if (this.headers['X-Github-Event'] !== 'pull_request') return this.body = 'not pull_request:' + this.headers['X-Github-Event']
   const reqJSON = JSON.parse(this.request.body)
   this.body = webhook(reqJSON, this.request.origin)
 })
